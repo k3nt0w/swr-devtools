2ba3028914360bba7cc67b841bc4246fb10425df
k3nt0w <kento.watanabe.11@gmail.com>
2021-04-26 15:41:55 +0900

    refactor: formatter

--- src/SWRDevtools/Data.tsx
+++ src/SWRDevtools/Data.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, memo } from "react";
-import themes from "./themes";
-import { DataProps } from "./types";
+import React, { useEffect, memo } from 'react'
+import themes from './themes'
+import { DataProps } from './types'
 
 export default memo(function Data({ JsonViewer, data, theme }: DataProps) {
   useEffect(() => {
-    JsonViewer.current = require("react-json-view").default;
-  }, []);
+    JsonViewer.current = require('react-json-view').default
+  }, [JsonViewer])
   return (
-    <div style={{ position: "relative", backgroundColor: "#222" }}>
+    <div style={{ position: 'relative', backgroundColor: '#222' }}>
       <div
         style={{
           padding: 0,
-          textAlign: "left",
-          overflowY: "scroll",
+          textAlign: 'left',
+          overflowY: 'scroll',
           maxHeight: 400,
-          boxSizing: "border-box",
-          ...themes[theme].data,
+          boxSizing: 'border-box',
+          ...themes[theme].data
         }}
       >
         <JsonViewer.current
@@ -23,9 +23,9 @@ export default memo(function Data({ JsonViewer, data, theme }: DataProps) {
           displayDataTypes={false}
           indentWidth={2}
           src={data || {}}
-          theme={theme === "Dark" ? "tube" : "shapeshifter:inverted"}
+          theme={theme === 'Dark' ? 'tube' : 'shapeshifter:inverted'}
         />
       </div>
     </div>
-  );
-});
+  )
+})
--- src/SWRDevtools/Panel/usePanelState.tsx
+++ src/SWRDevtools/Panel/usePanelState.tsx
@@ -1,60 +1,57 @@
-import { useEffect, useState, useCallback } from "react";
-import { useWindowSize, useStore } from "../hooks";
-import { UsePanelStateProps } from "../types";
+import React from 'react'
+import { useEffect, useState, useCallback } from 'react'
+import { useWindowSize, useStore } from '../hooks'
+import { UsePanelStateProps } from '../types'
 
-const getX = (windowWidth, width) => {
-  const x = windowWidth / 2 - width / 2;
-  return x;
-};
+const getX = (windowWidth: number, width: number) => {
+  const x = windowWidth / 2 - width / 2
+  return x
+}
 
-const getY = (windowHeight, height) => {
-  let y = 0;
-  if (typeof window !== "undefined") {
-    typeof window !== "undefined";
-    y = window.scrollY - windowHeight / 2 + height / 2;
+const getY = (windowHeight: number, height: number) => {
+  let y = 0
+  if (typeof window !== 'undefined') {
+    typeof window !== 'undefined'
+    y = window.scrollY - windowHeight / 2 + height / 2
   }
-  return y;
-};
+  return y
+}
 
-export default function usePanelState({ show }: UsePanelStateProps) {
-  const [theme, setTheme] = useState("Dark");
-  const { get, set, ready } = useStore({ store: "SWRDevtools" });
-  const { width: windowWidth, height: windowHeight } = useWindowSize();
-  const [width, setWidth] = useState(800);
-  const [height, setHeight] = useState(800);
+export default function usePanelState(_: UsePanelStateProps) {
+  const [theme, setTheme] = useState('Dark')
+  const { get, set, ready } = useStore({ store: 'SWRDevtools' })
+  const { width: windowWidth, height: windowHeight } = useWindowSize()
+  const [width, setWidth] = useState(800)
+  const [height, setHeight] = useState(800)
   //@ts-ignore
   const handleResize = useCallback(
-    async (
-      e: MouseEvent | TouchEvent,
-      dir: any,
-      refToElement: React.ElementRef<"div">
-    ) => {
-      await set("width", refToElement.offsetWidth);
-      setWidth(refToElement.offsetWidth);
-      await set("height", refToElement.offsetHeight);
-      setHeight(refToElement.offsetHeight);
+    async (_: MouseEvent | TouchEvent, __: any, refToElement: React.ElementRef<'div'>) => {
+      await set('width', refToElement.offsetWidth)
+      setWidth(refToElement.offsetWidth)
+      await set('height', refToElement.offsetHeight)
+      setHeight(refToElement.offsetHeight)
     },
-    [ready, set]
-  );
+    [set]
+  )
   const handleChangeTheme = useCallback(
-    async (theme) => {
-      await set("theme", theme);
-      setTheme(theme);
+    async theme => {
+      await set('theme', theme)
+      setTheme(theme)
     },
-    [set, show]
-  );
+    [set]
+  )
 
   useEffect(() => {
-    get("theme").then((theme) => {
-      if (theme) setTheme(theme);
-    });
-    get("width").then((width) => {
-      if (width) setWidth(width);
-    });
-    get("height").then((height) => {
-      if (height) setHeight(height);
-    });
-  }, [ready]);
+    get('theme').then(theme => {
+      if (theme) setTheme(theme)
+    })
+    get('width').then(width => {
+      if (width) setWidth(width)
+    })
+    get('height').then(height => {
+      if (height) setHeight(height)
+    })
+  }, [get, ready])
   return {
     theme,
     handleChangeTheme,
@@ -62,6 +59,6 @@ export default function usePanelState({ show }: UsePanelStateProps) {
     x: getX(windowWidth, width),
     y: getY(windowHeight, height),
     height,
-    width,
-  };
+    width
+  }
 }
